e5e3b9f9488f86cfa74dff2d8a5c07b2931a8ba1
Ofosu Osei <goofosuosei@gmail.com>
2023-09-16 00:42:13 +0100

    Initial commit

---
+++ myapp/scripts.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+def read_csv():
+    df = pd.read_csv("./dsets/automobiles.csv")
+    return df
+    pass
+
+def mpg_cat(mpg):
+    if mpg < 15:
+        return ("Low")
+    elif mpg < 25:
+        return ("Moderate")
+    else:
+        return ("High")
+    pass
+
+def my_stats(df):
+    return df.describe()
+    pass
